7b0958af50feb8cd1819a6da7ae867c2d3b63661
“meio” <meio134340@gmail.com>
2025-10-19 19:03:41 +0900

    地図の微調整

--- app/components/map.tsx
+++ app/components/map.tsx
@@ -13,15 +13,29 @@ export default function Map({ height = 400 }: Props) {
 
   // 白黒スタイル
   const grayscaleStyle: google.maps.MapTypeStyle[] = [
-    { elementType: "geometry", stylers: [{ color: "#f5f5f5" }] },
+    {
+      elementType: "geometry",
+      stylers: [
+        { color: "#000000ff" },
+        { saturation: -100 },
+        { lightness: -100 },
+      ],
+    },
     { elementType: "labels.icon", stylers: [{ visibility: "off" }] },
-    { elementType: "labels.text.fill", stylers: [{ color: "#616161" }] },
-    { elementType: "labels.text.stroke", stylers: [{ color: "#f5f5f5" }] },
+    { elementType: "labels.text.fill", stylers: [{ color: "#ffffffff" }] },
+    {
+      elementType: "labels.text.stroke",
+      stylers: [
+        { color: "#4f4f4fff" },
+        { saturation: -100 },
+        { lightness: -100 },
+      ],
+    },
     { featureType: "poi", stylers: [{ visibility: "off" }] },
     {
       featureType: "road",
       elementType: "geometry",
-      stylers: [{ color: "#ffffff" }],
+      stylers: [{ color: "#dcdcdc" }, { saturation: 100 }, { lightness: 100 }],
     },
     {
       featureType: "road.arterial",
