bff963a492ba76945dd3c0811820486d7cc6936b
“meio” <meio134340@gmail.com>
2026-05-18 22:00:14 +0900

    footerデザイン修正

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -5,22 +5,6 @@ export default function Footer() {
     <footer>
       <h2 className="sr-only">Access</h2>
       <Map />
-      <div>
-        <p>住所：福岡県北九州市小倉北区清水4-3-30</p>
-        <a
-          href="https://maps.app.goo.gl/42qEgAxmi3YHQxnf7"
-          rel="noopener noreferrer"
-          target="_blank"
-          className="info_link_map"
-        >
-          Google Maps
-        </a>
-        <br />
-        <a href="https://www.instagram.com/and_coffee_stand">
-          Instagram：
-          <span className="info_link_insta">@and_coffee_stand</span>
-        </a>
-      </div>
       <small>©︎and coffee stand ALL Rights Reserved.</small>
     </footer>
   );
