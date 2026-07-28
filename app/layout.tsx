84e878c2444d47c2877704cd753b54e789ea64df
“meio” <meio134340@gmail.com>
2026-07-28 23:40:24 +0900

    ガチ戻し

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,14 +51,11 @@ export const metadata: Metadata = {
     siteName: "And Coffee Stand",
     locale: "ja_JP",
     type: "website",
-    images: [
-      {
-        url: "/ogp.jpg",
-        width: 1200,
-        height: 630,
-        alt: "And Coffee Stand",
-      },
-    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "And Coffee Stand",
+    description: "女性が一人でも入りやすいお店──",
   },
   robots: {
     index: true,
